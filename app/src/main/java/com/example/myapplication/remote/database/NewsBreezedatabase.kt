83df960bb67856992d215a1d3ca441250893371e
Prachip9767 <prachip293@gmail.com>
2021-11-08 18:35:47 +0530

    Evaluation News Breeze

--- app/src/main/java/com/example/myapplication/remote/database/NewsBreezedatabase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.remote.database
-
-import androidx.room.Database
-
-@Database(entities = [NewsBreezeModel::class],version = 1)
-abstract class NewsBreezedatabase {
-    abstract fun getResponseFromDao():NewsBreezeDao
-}
